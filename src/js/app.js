c878d3502b429e885cbd3f58ff2eaa5cf1c4b378
cfen <chris.fenn@guardian.co.uk>
2017-10-04 14:41:01 +0100

    tidy up code

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import Swiper from 'swiper'
 import tracker from './tracker'
 import ScrollTrigger from './scrollTrigger'
 
-var isAndroidApp = (window.location.origin === "file://" && /(android)/i.test(navigator.userAgent) ) ? true : false;
+var isAndroidApp = (window.location.origin === "file://" && /(android)/i.test(navigator.userAgent)) ? true : false;
 
 const maxNoneSwipeH = 160;
 
@@ -16,22 +16,22 @@ function initSwiper() {
     console.log(cardStacks)
 
     for (var s = 0; s < cardStacks.length; s++) {
-// console.log(document.getElementByID("#pagination-"+s);
-      cardStacks[s].setAttribute('data-stack-position', s+1);
+        // console.log(document.getElementByID("#pagination-"+s);
+        cardStacks[s].setAttribute('data-stack-position', s + 1);
 
-// refs added to allow different lengths of swiper
-      var swiperTgt = document.querySelector(".swiperContainer"+s);
-      var paginateTgt = document.querySelector(".paginate"+s)
-     
-      var swiper = new Swiper(swiperTgt, {
-            paginationClickable: true,
-            loop: true,
-            slidesPerView: 1.2,
-            loopedSlides: cardStacks[s].length,
-            spaceBetween: 10,
-            pagination: paginateTgt ,
-            centeredSlides: true
-        })
+        // refs added to allow different lengths of swiper
+        var swiperTgt = document.querySelector(".swiperContainer" + s);
+        var paginateTgt = document.querySelector(".paginate" + s)
+
+        var swiper = new Swiper(swiperTgt, {
+                paginationClickable: true,
+                loop: true,
+                slidesPerView: 1.2,
+                loopedSlides: cardStacks[s].length,
+                spaceBetween: 10,
+                pagination: paginateTgt,
+                centeredSlides: true
+            })
         // .on('slideChangeEnd', function(currentSwiper, event) {
 
         //     swipers.forEach(function(s,i) {
@@ -40,26 +40,26 @@ function initSwiper() {
         //         var stackPosition = swiper.container[0].getAttribute('data-stack-position');
         //         analytics.registerEvent('stack_card_view', i)
         //       }
-            
+
         //         if (s.activeIndex != currentSwiper.activeIndex) {
         //           //s.activeIndex = currentSwiper.activeIndex;
         //             s.slideTo(currentSwiper.activeIndex, 0, false);
         //         } else {
-            
-            
+
+
         //         }
         //     });
         // })
         .on('onTouchStart', function(currentSwiper, e) {
-            if (isAndroidApp && window.GuardianJSInterface.registerRelatedCardsTouch) {
-                window.GuardianJSInterface.registerRelatedCardsTouch(true);
-            }
-        })
-        .on('onTouchEnd', function(currentSwiper, e) {
-            if (isAndroidApp && window.GuardianJSInterface.registerRelatedCardsTouch) {
-                window.GuardianJSInterface.registerRelatedCardsTouch(false);
-            }
-        });
+                if (isAndroidApp && window.GuardianJSInterface.registerRelatedCardsTouch) {
+                    window.GuardianJSInterface.registerRelatedCardsTouch(true);
+                }
+            })
+            .on('onTouchEnd', function(currentSwiper, e) {
+                if (isAndroidApp && window.GuardianJSInterface.registerRelatedCardsTouch) {
+                    window.GuardianJSInterface.registerRelatedCardsTouch(false);
+                }
+            });
 
         swipers.push(swiper);
 
@@ -74,18 +74,18 @@ var jumpLinks = document.querySelectorAll('[data-jump-to]'), i;
 
 for (i = 0; i < jumpLinks.length; ++i) {
     var jumpLink = jumpLinks[i];
-    jumpLink.addEventListener("click", function(e){
+    jumpLink.addEventListener("click", function(e) {
         e.preventDefault();
         var jumpTarget = this.getAttribute('data-jump-to');
-        var jumpDiv = document.querySelector('[id="group-'+jumpTarget+'"]');
+        var jumpDiv = document.querySelector('[id="group-' + jumpTarget + '"]');
         var body = document.documentElement || document.body || document.querySelector('body');
         var jumpBar = document.querySelector('.interactive-nav');
         var dadgummit = 5;
         var jumpOffset = body.scrollTop + jumpDiv.getBoundingClientRect().top - jumpBar.offsetHeight + dadgummit;
         //body.scrollTo(0, jumpOffset);
         // console.log(jumpOffset)
-        scrollTo(body, jumpOffset,  240);
-    },false);
+        scrollTo(body, jumpOffset, 240);
+    }, false);
 }
 
 function scrollTo(element, to, duration) {
@@ -104,7 +104,7 @@ function scrollTo(element, to, duration) {
 
 }
 
-function removeDisabled(){
+function removeDisabled() {
     let swipeSlides = document.querySelectorAll('.swiper-wrapper');
 
     for (var n = 0; n < swipeSlides.length; n++) {
@@ -117,16 +117,16 @@ var trigger = new ScrollTrigger({
     offset: { x: -50, y: 50 }
 });
 
-function initFullScrn(){
+function initFullScrn() {
 
     let exceededMaxH = getSlidesMaxH();
 
-    if(document.querySelector("body").clientWidth < 740 && exceededMaxH) {
-      // comment out for embeds
+    if (document.querySelector("body").clientWidth < 740 && exceededMaxH) {
+        // comment out for embeds
         initSwiper();
     }
 
-    if(document.querySelector("body").clientWidth > 740) {
+    if (document.querySelector("body").clientWidth > 740) {
         removeDisabled();
     }
 
@@ -134,38 +134,39 @@ function initFullScrn(){
 
 }
 
-function addListeners(){
+function addListeners() {
 
     document.querySelector('.gv-back-top-btn').addEventListener("click",
-        function(e){
+        function(e) {
             var jumpTarget = document.querySelector('.interactive-nav');
             var body = document.documentElement || document.body || document.querySelector('body');
             var dadgummit = 5;
             var jumpOffset = body.scrollTop + jumpTarget.getBoundingClientRect().top - jumpTarget.offsetHeight + dadgummit;
             //body.scrollTo(0, jumpOffset);
             // console.log(jumpOffset)
-            scrollTo(body, jumpOffset,  240);
+            scrollTo(body, jumpOffset, 240);
         }
     )
 
-    var Window =  window || document;
+    var Window = window || document;
 
     Window.addEventListener("scroll", WindowScrollListener);
 
 }
 
-function WindowScrollListener(){
+function WindowScrollListener() {
     //checkFixView()
 }
 
 
-function getSlidesMaxH(){
+function getSlidesMaxH() {
     let a = false;
     let swipeSlides = document.querySelectorAll('.swiper-slide');
 
-        for (var n = 0; n < swipeSlides.length; n++) {
-            if (swipeSlides[n].offsetHeight > maxNoneSwipeH){ a = true; console.log(swipeSlides[n].offsetHeight) };
-        }
+    for (var n = 0; n < swipeSlides.length; n++) {
+        if (swipeSlides[n].offsetHeight > maxNoneSwipeH) { a = true;
+            console.log(swipeSlides[n].offsetHeight) };
+    }
 
     return a;
 }
@@ -175,7 +176,7 @@ function checkFixView() {
     let navTop = document.querySelector(".interactive-nav").getBoundingClientRect().top;
     var pos_top = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
 
-    if (document.querySelector(".gv-hidden-footer")){
+    if (document.querySelector(".gv-hidden-footer")) {
         footTop = document.querySelector(".gv-hidden-footer").offsetTop;
     }
 
@@ -187,20 +188,19 @@ function checkFixView() {
     //     document.querySelector('.gv-back-top-btn').classList.add('hidden');
     // }
 
-        if( navTop < 0  && pos_top < (footTop - 240)){
-           document.querySelector('.gv-back-top-btn').classList.remove('hidden');
-        }
-        else if(pos_top > (footTop - 240) || navTop > 0 ){
-            document.querySelector('.gv-back-top-btn').classList.add('hidden');
-        }
+    if (navTop < 0 && pos_top < (footTop - 240)) {
+        document.querySelector('.gv-back-top-btn').classList.remove('hidden');
+    } else if (pos_top > (footTop - 240) || navTop > 0) {
+        document.querySelector('.gv-back-top-btn').classList.add('hidden');
+    }
 
 
-        // }else if (pos_top > navTop) {
-        //     document.querySelector('.gv-back-top-btn').classList.remove('hidden');
-        // }
-        // else if (pos_top < h) {
-        //     document.querySelector('.gv-back-top-btn').classList.add('hidden');
-        // }
+    // }else if (pos_top > navTop) {
+    //     document.querySelector('.gv-back-top-btn').classList.remove('hidden');
+    // }
+    // else if (pos_top < h) {
+    //     document.querySelector('.gv-back-top-btn').classList.add('hidden');
+    // }
 
 
 }
